3b9743aafa58569a4da7af120f13c08bc91bb20c
MrHacker26 <tjocz26@gmail.com>
2025-06-03 00:11:38 +0530

    feat(pkce): add functions to generate code verifier and challenge

---
+++ src/lib/pkce.ts
@@ -0,0 +1,18 @@
+export function generateCodeVerifier(length = 128): string {
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~'
+  const array = new Uint8Array(length)
+  crypto.getRandomValues(array)
+  return Array.from(array)
+    .map((x) => charset[x % charset.length])
+    .join('')
+}
+
+export async function generateCodeChallenge(verifier: string): Promise<string> {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(verifier)
+  const digest = await crypto.subtle.digest('SHA-256', data)
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '')
+}
